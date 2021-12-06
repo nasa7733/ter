e9c8b0d06c4d357aae69ba4844614e4fe723fee4
SrinuVadlapudi <87997023+SrinuVadlapudi@users.noreply.github.com>
2021-12-06 10:54:30 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+provider "google" {
+ 
+  project = "veera999"
+ // credentials = file("srinu2-14f292a3a4e3.json")
+  region  = "us-east1"
+  
+}
+
+resource "google_compute_instance" "vm-instance" {
+  name         = "test"
+  machine_type = "f1-micro"
+  zone         = "us-east1-b"
+
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+    
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
